f9499107d4cc2e11af216566d8f813d7cf07d1e4
kikikrestanto <kikikrestanto@gmail.com>
2025-02-25 15:03:27 +0700

    update ui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../src/styles/App.css';
-import image1 from '../src/assets/images/image-1.jpg';
-import image2 from '../src/assets/images/image-2.jpg';
 import image3 from '../src/assets/images/image-3.jpg';
+import logoHanaBank from '../src/assets/images/Logo Hana Bank.jpg';
+import logoAntamLM from '../src/assets/images/Logo Antam Logam Mulia.png';
+import logoHiBank from '../src/assets/images/Logo Hibank.png';
 import iconFacebook from '../src/assets/images/facebook.jpg';
 import iconInstagram from '../src/assets/images/instagram.jpg';
 import iconTwitter from '../src/assets/images/twitter.jpg';
@@ -10,9 +11,6 @@ import iconLinkedln from '../src/assets/images/Linkedin.jpg';
 import apiClient from './services';
 
 function App() {
-
-    const [informationDetail, setInformationDetail] = React.useState([]);
-
     React.useEffect(function () {
         const fetchData = async () => {
             try {
@@ -26,19 +24,25 @@ function App() {
         fetchData();
     }, []);
 
+
+    const listMyWorks = [
+        {id: 1, title: 'Hana Bank' , description: 'test', image: logoHanaBank, date: '2022', tools : 'tools'},
+        {id: 2, title: 'Antam Logam Mulia' , description: 'test', image: logoAntamLM, date: '2023', tools : 'tools'},
+        {id: 3, title: 'Hi Bank' , description: 'test', image: logoHiBank, date: '2023', tools : 'tools'}
+    ];
+    
+    const listEducation = [
+        {id: 1, name: 'Gunadarma University, Depok', time: '2017 - 2021',  major: 'Information System' ,description: 'Final years student, study Information System with GPA 3.31 and will graduate in 2021.'},
+        {id: 2, name: 'SMA Negeri 9 Jakarta', time: '2013 - 2016',major: 'Social Scince' , description: '-'},
+    ]
+
   return (
     <React.Fragment>
-      <header>
-        <a href="#">Works</a>
-        <a href="#">Blog</a>
-        <a href="#">Contact</a>
-      </header>
-
       <main>
       <div class="section-one">
             <div class="introduction">
                 <div class="title">
-                    <p>Hi, Im john doe,
+                    <p>Kiki Krestanto,
                         <br/>
                         Frontend Developer
                     </p>
@@ -58,49 +62,24 @@ function App() {
         <div class="section-two">
             <div class="blog">
                 <div class="activity">
-                    <p>Recent Posts</p>
-                    <p class="view">View All</p>
+                    <p>Educations</p>
                 </div>
                 <div class="content-card">
-                    <div class="card">
-                        <p>Making a design system from scratch</p>
-                        <div class="time">
-                            <p>12 Feb 2020</p>
-                            &nbsp;
-                            <p>|</p>
-                            &nbsp;
-                            <p>Design, Pattern</p>
+                    {listEducation.map((item) => 
+                        <div class="card" key={item.id}>
+                            <p>{item.name}</p>
+                            <div class="time">
+                                <p>{item.time}</p>
+                                &nbsp;
+                                <p>|</p>
+                                &nbsp;
+                                <p>{item.major}</p>
+                            </div>
+                            <p>
+                            {item.description}
+                            </p>
                         </div>
-                        <p>
-                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                        </p>
-                    </div>
-                    <div class="card">
-                        <p>Making a design system from scratch</p>
-                        <div class="time">
-                            <p>12 Feb 2020</p>
-                            &nbsp;
-                            <p>|</p>
-                            &nbsp;
-                            <p>Design, Pattern</p>
-                        </div>
-                        <p>
-                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                        </p>
-                    </div>
-                    <div class="card">
-                        <p>Making a design system from scratch</p>
-                        <div class="time">
-                            <p>12 Feb 2020</p>
-                            &nbsp;
-                            <p>|</p>
-                            &nbsp;
-                            <p>Design, Pattern</p>
-                        </div>
-                        <p>
-                            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
-                        </p>
-                    </div>
+                    )}
                 </div>
                 
             </div>   
@@ -108,43 +87,26 @@ function App() {
 
         <div class="section-three">
             <div class="list-title">
-                <p>Feature Works</p>
+                <p>My Works</p>
             </div>
-            
-            <div class="list">
-                <img src={image1} alt="image-1" class="image-list"/>
+            {listMyWorks.map((item) => 
+            <div class="list" key={item.id}>
+                <img src={item.image} alt="image-1" class="image-list"/>
                 <div class="dashboard-list">
-                    <span class="dashboard-title">Designing Dashboards</span>
+                    <span class="dashboard-title">{item.title}</span>
                     &nbsp;
                     <div class="dashboard-year-and-name">
-                        <span class="dashboard-year">2020</span>
-                        <span class="dashboard-name">Design</span>
+                        <span class="dashboard-year">{item.date}</span>
+                        <span class="dashboard-name">{item.name}</span>
                     </div>
                     &nbsp;
                     <div class="dashboard-decription">
-                        <span>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
+                        <span>{item.description}</span>
                     </div>
-                </div>
+                    
+                </div>    
             </div>
-            <hr/>
-
-            <div class="list">
-                <img src={image2} alt="image-1" class="image-list"/>
-                <div class="dashboard-list">
-                    <span class="dashboard-title">Vibrant Portraits of 2020</span>
-                    &nbsp;
-                    <div class="dashboard-year-and-name">
-                        <span class="dashboard-year">2018</span>
-                        <span class="dashboard-name">Ilustration</span>
-                    </div>
-                    &nbsp;
-                    <div class="dashboard-decription">
-                        <span>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.</span>
-                    </div>
-                </div>
-            </div>
-            <hr/>
-
+            )}
             <div class="list">
                 <img src={image3} alt="image-1" class="image-list"/>
                 <div class="dashboard-list">
